eb1cabef6ea914c2d9db4755d01c4ae0687f62b3
devanojj <devanojose4@gmail.com>
2025-08-02 22:04:01 +0100

    Day Two: Expense Tracker JSX

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,8 +1,31 @@
+import {auth, provider} from "../../config/firebase-config";
+import {signInWithPopup} from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
 export const Auth = () => {
+  const navigate = useNavigate();
+
+
+  const signInWithGoogle = async () => {
+    const results = await signInWithPopup(auth, provider);
+    console.log(results);
+    const authInfo = {
+      userID: results.user.uid,
+      user: results.user.displayName,
+      profilePhoto: results.user.photoURL,
+      isAuth: true
+    }
+    localStorage.setItem("auth", JSON.stringify(authInfo));
+    navigate("/expense-tracker");
+  }
+
   return (
     <div className="login-page">
-      
-     Hello
+      <p>Sign In With Google to Continue</p>
+      <button className="login-with-google-btn" onClick={signInWithGoogle}>
+        {""}
+        Sign In With Google
+      </button>
     </div>
   );
 }
